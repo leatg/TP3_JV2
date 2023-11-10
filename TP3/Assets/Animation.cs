d1fe8f54de6e03ea6f094898d5065266386c1337
leatg <95499860+leatg@users.noreply.github.com>
2023-11-10 10:53:24 -0500

    debut projet

---
+++ TP3/Assets/Animation.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class Animation : MonoBehaviour
+{
+    
+    Animator animator;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W)){
+            //animator.SetBool(0, true); //IsWalking
+            animator.SetBool("IsWalking", true);
+        }
+        else if (Input.GetKeyUp(KeyCode.W)){
+            animator.SetBool("IsWalking", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            //animator.SetBool(0, true); //IsWalking
+            animator.SetBool("IsRunning", true);
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            animator.SetBool("IsRunning", false);
+        }
+    }
+}
